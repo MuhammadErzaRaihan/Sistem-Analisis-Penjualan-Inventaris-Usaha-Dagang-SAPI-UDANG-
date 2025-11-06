0e59a81985402b4150ac4fb845464b20a7c850df
rizal <rizalfikri256@gmail.com>
2025-11-06 14:02:23 +0800

    tahap-1 patch 2

---
+++ app/Language/en/Login.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'title' => 'SADANG THJ Dashboard',
+    'subtitle' => 'Welcome back! Please enter your details.',
+    'employee_only' => 'This page is for employees/internal staff only',
+    'go_to_homepage' => 'Back to Homepage',
+    'email' => 'Email',
+    'email_placeholder' => 'Enter your email',
+    'password' => 'Password',
+    'password_placeholder' => '**********',
+    'remember_me' => 'Remember me',
+    'forgot_password' => 'Forgot password',
+    'login_button' => 'Login',
+    'switch_to_indonesian' => 'Indonesian',
+];
+
---
+++ app/Language/id/Login.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'title' => 'SADANG THJ Dashboard',
+    'subtitle' => 'Selamat datang kembali! Silakan masukkan detail Anda.',
+    'employee_only' => 'Halaman ini khusus untuk karyawan/internal toko',
+    'go_to_homepage' => 'Kembali ke Homepage',
+    'email' => 'Email',
+    'email_placeholder' => 'Masukkan email Anda',
+    'password' => 'Kata Sandi',
+    'password_placeholder' => '**********',
+    'remember_me' => 'Ingat saya',
+    'forgot_password' => 'Lupa kata sandi',
+    'login_button' => 'Masuk',
+    'switch_to_english' => 'English',
+];
+
